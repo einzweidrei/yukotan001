f6e137cc8865174c756d9d2a93f51575b8019a24
einzweidrei <einzweidrei2@gmail.com>
2017-06-01 01:05:18 +0700

    Create
    Up

---
+++ yukotan001/src/app/pages/tables/components/smartTables/smartTables.component.ts
@@ -0,0 +1,102 @@
+import { Component } from '@angular/core';
+
+import { SmartTablesService } from './smartTables.service';
+import { LocalDataSource } from 'ng2-smart-table';
+
+@Component({
+  selector: 'smart-tables',
+  templateUrl: './smartTables.html',
+  styleUrls: ['./smartTables.scss']
+})
+export class SmartTables {
+
+  query: string = '';
+
+  settings = {
+    add: {
+      addButtonContent: '<i class="ion-ios-plus-outline"></i>',
+      createButtonContent: '<i class="ion-checkmark"></i>',
+      cancelButtonContent: '<i class="ion-close"></i>',
+      confirmCreate: true,
+    },
+    edit: {
+      editButtonContent: '<i class="ion-edit"></i>',
+      saveButtonContent: '<i class="ion-checkmark"></i>',
+      cancelButtonContent: '<i class="ion-close"></i>',
+      confirmSave: true,
+    },
+    delete: {
+      deleteButtonContent: '<i class="ion-trash-a"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      id: {
+        title: 'ID',
+        type: 'number',
+      },
+      firstName: {
+        title: 'First Name',
+        type: 'string',
+      },
+      lastName: {
+        title: 'Last Name',
+        type: 'string',
+      },
+      username: {
+        title: 'Username',
+        type: 'string',
+      },
+      email: {
+        title: 'E-mail',
+        type: 'string',
+      },
+      age: {
+        title: 'Age',
+        type: 'number',
+      }
+    }
+  };
+
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor(protected service: SmartTablesService) {
+    this.service.getData().then((data) => {
+      this.source.load(data);
+    });
+  }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
+
+  onSaveConfirm(event) {
+    window.console.log(event);
+    window.console.log('click save');
+
+    // this.service.getData().then((data) => {
+    //   this.source = new LocalDataSource();
+    // });
+
+    window.console.log('reloading');
+
+    // if (window.confirm('Are you sure you want to save?')) {
+    //   event.newData['name'] += ' + added in code';
+    //   event.confirm.resolve(event.newData);
+    // } else {
+    //   event.confirm.reject();
+    // }
+  }
+
+  onCreateConfirm(event) {
+    if (window.confirm('Are you sure you want to create?')) {
+      event.newData['name'] += ' + added in code';
+      event.confirm.resolve(event.newData);
+    } else {
+      event.confirm.reject();
+    }
+  }
+}
